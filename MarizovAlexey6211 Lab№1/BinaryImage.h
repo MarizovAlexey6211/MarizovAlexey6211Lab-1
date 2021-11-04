613a97a4b7ef54b6cf99095b488d8d7343760afe
mar.karnage@outlook.com <mar.karnage@outlook.com>
2021-11-04 19:41:29 +0400

    Added a class

---
+++ MarizovAlexey6211 Lab№1/BinaryImage.h
@@ -0,0 +1,10 @@
+#ifndef BinaryImage_h
+#define BinaryImage_h
+class BinaryImage {
+private:
+    bool** data;
+    int cols;
+    int rows;
+};
+
+#endif /* BinaryImage_h */
---
+++ MarizovAlexey6211 Lab№1/Source.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<time.h>
+using namespace std;
+#include"BinaryImage.h"
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+}
