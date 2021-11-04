1a4d679b7d2c2b6a504b494490f4b39d6810325d
MarizovAlexey6211 <mar.karnage@outlook.com>
2021-11-04 23:16:29 +0400

    feat:copy constructor,destructor,fill factor function

--- MarizovAlexey6211 Lab№1/BinaryImage.h
+++ MarizovAlexey6211 Lab№1/BinaryImage.h
@@ -11,6 +11,31 @@ public:
         for (int i = 0; i < rows; ++i)
             data[i] = new bool[cols];
     }
+    BinaryImage(const BinaryImage& rhs) : BinaryImage(rhs.cols, rhs.rows) {
+        for (int i = 0; i < rows; ++i)
+            for (int j = 0; j < cols; ++j)
+                data[i][j] = rhs.data[i][j];
+
+    }
+    ~BinaryImage() {
+        if (data) {
+            for (int i = 0; i < rows; ++i)
+                if (data[i])
+                    delete[] data[i];
+            delete[] data;
+        }
+    }
+
+    double Ratio()
+    {
+        int count = 0;
+        for (int i = 0; i < rows; ++i)
+            for (int j = 0; j < cols; ++j)
+                if (data[i][j] == true)
+                    count++;
+        return (double)count / (rows * cols);
+    }
+
 };
 
 #endif /* BinaryImage_h */
